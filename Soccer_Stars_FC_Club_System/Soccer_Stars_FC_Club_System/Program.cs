adeea90a4af133db9f8a342c54ae1409579d1f91
hakimyusuf48 <129780930+hakimyusuf48@users.noreply.github.com>
2024-06-06 11:56:53 +0200

    Add files via upload

---
+++ Soccer_Stars_FC_Club_System/Soccer_Stars_FC_Club_System/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Question_1 {
+    class Program {
+        static void Main(string[] args)
+        {
+            MenuCommand;
+        }
+
+        static void AddPlayer() {
+            //Player Information
+            Console.WriteLine("Enter player details");
+            Console.WriteLine("Name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Position: ");
+            String position = Console.ReadLine();
+            Console.WriteLine("Player added successfully!");
+        }
+
+        static void RemovePlayer() {
+        }
+
+        static void SearchPlayer() {
+        }
+
+        static void DisplayPlayers() {
+        }
+    }
+}
